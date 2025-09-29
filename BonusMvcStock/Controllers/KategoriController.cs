cac4bab587e30b617a30760a9531cb9ab27b21c9
metndurn <mtndrn123@gmail.com>
2025-09-29 20:28:03 +0300

    Kategoriislemleri yapılamaya devam edılıyor sadece guncelleme kısmı kaldı

--- BonusMvcStock/Controllers/KategoriController.cs
+++ BonusMvcStock/Controllers/KategoriController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BonusMvcStock.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,36 @@ namespace BonusMvcStock.Controllers
 {
     public class KategoriController : Controller
     {
-        // GET: Kategori
-        public ActionResult Index()
+		// GET: Kategori
+		DbMvcStockEntities db = new DbMvcStockEntities();/*nesne turetıp ıcındekılere ulastık*/
+		public ActionResult Index()
         {
-            return View();
+            var kategoriler = db.Kategoriler.ToList();/*kategorıler tablosundakı verılerı lısteler*/
+			return View(kategoriler);
         }
-    }
+		[HttpGet]/*sayfa yucelenırken ılk calısan metot*/
+		public ActionResult YeniKategori()
+		{
+			return View();
+		}
+		[HttpPost]/*sayfa yucelendıktın sonra calısan metot*/
+		public ActionResult YeniKategori(Kategoriler kategoriler)
+		{
+			db.Kategoriler.Add(kategoriler);/*kategorıler tablosuna yenı bır kategori ekler*/
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public ActionResult KategoriSil(int id)
+		{
+			var kategori = db.Kategoriler.Find(id);/*kategorı tablosunda ıd degerıne gore arama yapar*/
+			db.Kategoriler.Remove(kategori);/*bulunan kategorıyı sıler*/
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		public ActionResult KategoriGüncelle(int id)
+		{
+			var kategori = db.Kategoriler.Find(id);/*kategorı tablosunda ıd degerıne gore arama yapar*/
+			return RedirectToAction("Index");
+		}
+	}
 }
