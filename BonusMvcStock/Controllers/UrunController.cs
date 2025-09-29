2a203f8f13f4e9b7c54a1186f424df51b7fba873
metndurn <mtndrn123@gmail.com>
2025-09-29 21:43:26 +0300

    urunler kısmına gecıldı ve devamı var suanlık sadece lısteleme ve ekleme yapıldı

---
+++ BonusMvcStock/Controllers/UrunController.cs
@@ -0,0 +1,32 @@
+﻿using BonusMvcStock.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BonusMvcStock.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        DbMvcStockEntities db = new DbMvcStockEntities();/*nesne turetıp ıcındekılere ulastık*/
+		public ActionResult Index()
+        {
+            var urunler = db.Urunler.ToList();
+			return View(urunler);
+        }
+        [HttpGet]/*sayfa yucelenırken ılk calısan metot*/
+        public ActionResult YeniUrun()
+        {
+            return View();
+		}
+        [HttpPost]
+		public ActionResult YeniUrun(Urunler urunler)/*parametreli metod*/
+		{
+			db.Urunler.Add(urunler);/*urunler tablosuna yenı bır urun ekler*/
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
