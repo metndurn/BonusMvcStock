fbe2deedac495b26399bfc7bc6e369d5676dfefd
metndurn <mtndrn123@gmail.com>
2025-11-04 01:03:11 +0300

    admin isleminde popup kullanıp kullanıcı ekledık

---
+++ BonusMvcStock/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using BonusMvcStock.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BonusMvcStock.Controllers
+{
+	public class AdminController : Controller
+	{
+		// GET: Admin
+		DbMvcStockEntities db = new DbMvcStockEntities();/*nesne turetıp ıcındekılere ulastık*/
+		public ActionResult Index()
+		{
+			return View();
+		}
+		[HttpGet]
+		public ActionResult YeniAdmin()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult YeniAdmin(Admin admin)
+		{
+			db.Admin.Add(admin);/*admin tablosuna yenı bır admin ekler*/
+			db.SaveChanges();
+			return RedirectToAction("Index");
+
+			//var admn = db.Admin.FirstOrDefault(x => x.Kullanici == admin.Kullanici && x.Sifre == admin.Sifre);
+			//if (admn != null)
+			//{
+			//	Session["Kullanici"] = admn.Kullanici.ToString();
+			//	return RedirectToAction("Index", "Urun");
+			//}
+			//else
+			//{
+			//	return RedirectToAction("YeniAdmin", "Admin");
+			//}
+		}
+	}
+}
